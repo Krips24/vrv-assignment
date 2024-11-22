11cbeedcc9ae1243449564abdbee5f096d5aae9f
krapansh <krapansh24@gmail.com>
2024-11-22 22:22:24 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,23 @@ export default function Home() {
           <p className="mt-2 text-gray-600">
             Use the sidebar to navigate through users, roles, and permissions.
           </p>
+
+          <div className="mt-44">
+            <p className="mt-12 text-gray-600">
+              This app is made by Krapansh Shrivastava, a full-stack developer.
+              Currently I am working as a Full Stack Develope at IIT Indore,
+              India.
+            </p>
+
+            <p className="mt-2 text-gray-600">
+              Happy to complete the project. Thanks for using this app.
+            </p>
+
+            <p className="mt-2 text-gray-600">
+              If you liked this app, please feel free to contact me at
+              krapansh.work@gmail.com or +91 6281284310
+            </p>
+          </div>
         </section>
       </main>
     </div>
